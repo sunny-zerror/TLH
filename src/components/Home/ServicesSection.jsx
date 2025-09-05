b20f2845cd82888f0bf7a2008c7ce56001b050d4
sunny-zerror <sunny.zerror@gmail.com>
2025-09-05 16:29:06 +0530

    new ui of service section

--- src/components/Home/ServicesSection.jsx
+++ src/components/Home/ServicesSection.jsx
@@ -1,11 +1,10 @@
-import { RiArrowRightUpLine, RiMapPinFill } from '@remixicon/react'
+import ServiceBtn from '@/components/Buttons/ServiceBtn';
+import { RiMapPinFill } from '@remixicon/react';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/dist/ScrollTrigger';
 import React, { useEffect, useState } from 'react'
 
-import gsap from "gsap";
-import ScrollTrigger from "gsap/dist/ScrollTrigger";
-import SplitText from 'gsap/dist/SplitText';
-import ServiceBtn from '../Buttons/ServiceBtn';
-gsap.registerPlugin(ScrollTrigger, SplitText);
+gsap.registerPlugin(ScrollTrigger)
 
 const services = [
     {
@@ -29,7 +28,7 @@ const services = [
     {
         img: "/images/services/1_Garment Care.webp",
         title: "Garment Care",
-        desc: "The Laundry House offers premium garment care services designed to preserve the quality, feel, and freshness of every piece. Whether it's daily wear, delicate fabrics, or designer outfits, we handle each garment with expert attention and care. Our services include professional washing, stain removal, fabric-specific cleaning, ironing, and finishing – all tailored to suit your clothing’s needs.",
+        desc: "Premium garment care tailored for daily wear, delicate fabrics, and designer outfits. From washing and stain removal to fabric-specific cleaning and finishing, we ensure every piece stays fresh and refined.",
         cities: ["Delhi", "Mumbai", "Bengaluru", "Hyderabad", "Kolkata"],
     },
 
@@ -38,171 +37,98 @@ const services = [
 ];
 
 
-const ServicesSection = () => {
-    const [showAllCities, setShowAllCities] = useState({}); // track cities expanded per service
+const index = () => {
+
+    const [showAllCities, setShowAllCities] = useState({});
 
     const toggleShowAll = (index) => {
         setShowAllCities((prev) => ({
             ...prev,
-            [index]: !prev[index], // toggle only for this slide
+            [index]: !prev[index],
         }));
     };
 
     useEffect(() => {
-        if (window.innerWidth < 1024) return
-        const ctx = gsap.context(() => {
-            const split1 = new SplitText(".service_animate_txt_a", { type: "words" });
-            const split2 = new SplitText(".service_animate_txt_b", { type: "words" });
-            const split3 = new SplitText(".service_animate_txt_c", { type: "words" });
-            const split4 = new SplitText(".service_animate_txt_d", { type: "words" });
-            const split5 = new SplitText(".service_animate_txt_e", { type: "words" });
 
-            const tl = gsap.timeline({
-                scrollTrigger: {
-                    trigger: ".services_parent",
-                    start: "top 60%",
-                    // markers: true,
-                    toggleActions: "play none none reverse",
-                }
-            })
-
-            // tl.fromTo(
-            //     [split1.words, split2.words],
-            //     { y: 20, opacity: 0 },
-            //     {
-            //         y: 0,
-            //         opacity: 1,
-            //         duration: 0.5,
-            //         stagger: 0.05,
-            //     }, "parallel");
-
-            // tl.fromTo(
-            //     ".border_anim",
-            //     { width: 0 },
-            //     {
-            //         width: "100%",
-            //         duration: 1,
-            //     }, "parallel");
-            // tl.fromTo(
-            //     ".slide_bg",
-            //     { backgroundColor: "transparent" },
-            //     {
-            //         backgroundColor: "#9494941f",
-            //         opacity: 1,
-            //         duration: 1,
-            //     }, "parallel");
-            tl.fromTo(
-                ".service_clip_img",
-                {
-                    clipPath: "polygon(0 0, 100% 0, 100% 0, 0 0)",
-                    // filter: "blur(5px)"
-                },
-                {
-                    delay: 0.2,
-                    ease: "power2.inOut",
-                    clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-                    // filter: "blur(0px)",
-                    duration: 1,
-                }, "parallel");
-
-            // tl.fromTo(
-            //     [split3.words, split4.words, split5.words, ".service_icon", ".service_btn"],
-            //     { y: 10, opacity: 0 },
-            //     {
-            //         y: 0,
-            //         opacity: 1,
-            //         delay: 0.2,
-            //         duration: 0.5,
-            //         stagger: 0.005,
-            //     }, "parallel");
-
-            if (window.innerWidth < 1024) return
-
-        var tl2 = gsap.timeline({
+        var tl = gsap.timeline({
             scrollTrigger: {
-                trigger: '.services_parent',
-                pin:true,
-                scrub:true,
-                start:"top top",
-                // markers:true,
-                end:window.innerHeight*4
+                trigger: ".service_paren",
+                start: "top top",
+                scrub: true,
+                pin: true,
+                end: "+=3000",
+                // markers: true,
             }
         })
-        tl2.to(".slide_bg_0",{
-            scale:0.8,
-        },"par")
-        tl2.to(".slide_bg_1",{
-            top:0,
-        },"par")
-        tl2.to(".slide_bg_1",{
-            scale:0.8,
-        },"par2")
-        tl2.to(".slide_bg_2",{
-            top:0,
-        },"par2")
-        tl2.to(".slide_bg_2",{
-            scale:0.8,
-        },"par3")
-        tl2.to(".slide_bg_3",{
-            top:0,
-            zIndex:10,
-        },"par3")
 
-        });
+        tl.to(".slide_service", {
+            translateY: "-300vh",
+            duration: 3,
+            ease: "none",
+        }, "we")
+        tl.to(".serv_img_3", {
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+            duration: 1,
+            ease: "none",
+        }, "we")
+        tl.to(".serv_img_2", {
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+            delay: 1,
+            duration: 1,
+            ease: "none",
+        }, "we")
+        tl.to(".serv_img_1", {
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+            delay: 2,
+            duration: 1,
+            ease: "none",
+        }, "we")
 
-        return () => ctx.revert();
-    }, []);
+    }, [])
 
 
     return (
-        <div className=' hidden lg:flex services_parent my-16 lg:my-0  lg:h-screen w-full items-center justify-center  '>
-            <div className="   lg:h-[37vw]  w-full overflow-hidden relative">
-                {
-                    services.map((item, index) => {
-                        const showAll = showAllCities[index] || false;
-                        const visibleCities = showAll ? item?.cities : item?.cities.slice(0, 4);
-                        return (
-                            <div key={index} className={` slide_bg_${index}  ${index === 0 ? "top-0" : "top-[100%]"}  mt-10 lg:mt-0 z-[${index}] blur-[0px]  w-full h-full px-5 lg:px-10 bg-[#FFFAF0] lg:absolute  left-0 overflow-hidden`}>
-                                <div className={` slide_inn_${index}  slide_bg  w-full h-full p-5 lg:p-14 flex flex-col-reverse md:flex-row items-center justify-between bg-[#9494941f] rounded-xl`}>
-                                    <div className=" w-full mt-5 md:mt-0 md:w-[45%] h-full flex flex-col justify-between">
-                                        <p className='service_animate_txt_a  md:mb-5 opacity-80'>Our Services</p>
+        <div>
+            <div className=" hidden lg:block service_paren my-16  w-full h-screen  relative ">
+                <div className="w-full slide_service">
+                    {
+                        services.map((item, index) => {
+                            const showAll = showAllCities[index] || false;
+                            const visibleCities = showAll ? item?.cities : item?.cities.slice(0, 4);
+                            return (
+                                <div key={index} className={`  h-[100vh]   w-full px-5 lg:px-10  overflow-hidden`}>
+                                    <div className=" w-full mt-5 md:mt-0 md:w-[45%] p-5 lg:p-14 h-full flex flex-col justify-center">
+                                        <p className='service_animate_txt_a  opacity-80 mb-5'>Our Services</p>
 
-                                        <div className="w-full flex flex-col gap-4 justify-between h-full ">
+                                        <div className="w-full flex flex-col gap-[5vw] justify-center  ">
                                             <div className="">
-                                                <p className='text-3xl lg:text-6xl  service_animate_txt_b'>{item.title}</p>
+                                                <p className='text-3xl lg:text-6xl  font-semibold service_animate_txt_b'>{item.title}</p>
                                                 <div className=" border_anim  w-full origin-left md:mt-2 border  border-dashed rounded-full border-black/30"></div>
                                             </div>
                                             <p className=' text-xs lg:text-base  service_animate_txt_c'>{item.desc}</p>
 
                                             <div className="stores">
-                                                <div className="flex md:flex-col lg:flex-row  lg:items-center gap-2">
+                                                <div className="flex md:flex-col lg:flex-row  lg:items-center gap-1">
                                                     <div className="flex items-center gap-2">
                                                         <RiMapPinFill size={18} className='service_icon translate-y-[-2px]' />
-                                                        <p className=' service_animate_txt_d text-xs lg:text-sm uppercase  '>Locate Stores:</p>
+                                                        <p className=' service_animate_txt_d text-xs lg:text-sm font-semibold uppercase  '>Locate Stores:</p>
                                                     </div>
                                                     <p className=' service_animate_txt_e text-xs lg:text-sm opacity-70'>Click the location below to explore</p>
                                                 </div>
                                                 <div className="w-full flex flex-wrap mt-4 gap-2 ">
                                                     {visibleCities.map((store, i) => (
-                                                        <button
-                                                            key={i}
-                                                            className="text-xs lg:text-base service_btn relative overflow-hidden group rounded-full border-2 border-[#0e1111] px-4 center   py-1 lg:py-1.5"
-                                                        >
-                                                            <p className="   opacity-0">{store}</p>
-                                                            <p className=" group-hover:translate-y-[-10px] group-hover:opacity-0 transition-all duration-300   absolute">
-                                                                {store}
-                                                            </p>
-                                                            <div className="w-full group-hover:scale-110 origin-center group-hover:top-0 transition-all duration-300 h-full black left-0 top-[100%] absolute rounded-full"></div>
-                                                            <p className="  translate-y-[10px] z-[99] text-white group-hover:translate-y-[0px] group-hover:opacity-100 opacity-0 transition-all duration-300   absolute">
-                                                                {store}
-                                                            </p>
+                                                        <button key={i} className=' text-sm lg:text-base learn_btn relative overflow-hidden group  text-white lg:text-black   bg-black lg:bg-transparent  rounded-full border-1 border-[#0e1111] px-0 py-2 lg:px-4 center lg:py-1.5'>
+                                                            <p className='fixy1 font-normal opacity-0'>{store}</p>
+                                                            <p className='lg:fixy1 group-hover:translate-y-[-10px] group-hover:opacity-0 transition-all duration-300 font-normal absolute'>{store} </p>
+                                                            <div className="w-full origin-center group-hover:left-0 transition-all duration-300 h-full black top-0 left-[-100%] absolute "></div>
+                                                            <p className='fixy1 font-normal translate-y-[10px] z-[99] text-white group-hover:translate-y-[0px] group-hover:opacity-100 opacity-0 transition-all duration-300  absolute'> {store}</p>
                                                         </button>
                                                     ))}
 
                                                     {item?.cities.length > 3 && (
                                                         <button
                                                             onClick={() => toggleShowAll(index)}
-                                                            className="text-xs lg:text-base service_btn relative overflow-hidden group rounded-full border-2 border-[#0e1111] px-4 center   py-1 lg:py-1.5"
+                                                            className="text-xs lg:text-base hover:bg-black/5  transition duration-300 service_btn relative overflow-hidden group rounded-full border border-[#0e1111] px-4 center   py-1 lg:py-1.5"
                                                         >
                                                             <p className="     z-[99]">
                                                                 {showAll ? "show less" : ` + ${item?.cities.length - 4} more`}
@@ -213,25 +139,31 @@ const ServicesSection = () => {
                                             </div>
 
                                             <div className="w-full ">
-                                                <ServiceBtn/>
+                                                <ServiceBtn />
                                             </div>
 
                                         </div>
                                     </div>
-                                    <div className=" w-full md:w-[40%]  h-full">
-                                        <div
-                                            className=" service_clip_img  w-full h-full overflow-hidden rounded-xl">
-                                            <img className='w-full h-full object-cover' src={item.img} alt="" />
-                                        </div>
-                                    </div>
                                 </div>
+                            )
+                        })
+                    }
+                </div>
+
+                <div className="absolute z-[2] w-[40%] h-[75vh]  overflow-hidden rounded-xl top-1/2 right-24 -translate-y-1/2 ">
+                    {
+                        services.slice().reverse().map((item, index) => (
+                            <div
+                                style={{ clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)" }}
+                                key={index} className={` serv_img_${index} absolute rounded-xl overflow-hidden w-full h-full top-0 left-0  `}>
+                                <img src={item.img} alt={item.title} className='w-full rounded-xl h-full object-cover' />
                             </div>
-                        )
-                    })
-                }
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
 }
 
-export default ServicesSection
+export default index
